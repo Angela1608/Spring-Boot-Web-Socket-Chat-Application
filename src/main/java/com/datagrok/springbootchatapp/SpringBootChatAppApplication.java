c4312f17bb8e09e74bb307847be96586df95f279
Angela Matiiva <angelamatiiva@gmail.com>
2023-02-06 20:14:33 +0200

    first commit

---
+++ src/main/java/com/datagrok/springbootchatapp/SpringBootChatAppApplication.java
@@ -0,0 +1,12 @@
+package com.datagrok.springbootchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChatAppApplication.class, args);
+    }
+}
---
+++ src/main/java/com/datagrok/springbootchatapp/controller/ChatController.java
@@ -0,0 +1,25 @@
+package com.datagrok.springbootchatapp.controller;
+
+import com.datagrok.springbootchatapp.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Message addUser(@Payload Message message,
+                           SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+}
